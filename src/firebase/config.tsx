7f2646cddebd9ded158d0d88853927b1ca82c61e
Shamsur Raza Chowdhury <shamsurraza.chowdhury@tuni.fi>
2022-02-02 21:15:42 +0200

    added firebase and firestore

---
+++ src/firebase/config.tsx
@@ -0,0 +1,17 @@
+import { FirebaseApp, initializeApp } from "firebase/app";
+import {Firestore, getFirestore} from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB9q3uutSGndSFaPlHRnoGE_cVbmGSjDWc",
+  authDomain: "book-c72be.firebaseapp.com",
+  projectId: "book-c72be",
+  storageBucket: "book-c72be.appspot.com",
+  messagingSenderId: "505018773655",
+  appId: "1:505018773655:web:a3474c1357ad94ff6539d2"
+};
+
+// init firebase 
+export const app: FirebaseApp = initializeApp(firebaseConfig);
+
+//init firestore 
+export const db: Firestore = getFirestore();
