4e19a6aa63a40f27ee6d86b0b9baca843d61c581
Shamsur Raza Chowdhury <shamsurraza.chowdhury@tuni.fi>
2022-02-04 04:44:17 +0200

    adding navbar

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import { Route, Routes, BrowserRouter } from "react-router-dom"
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import { useAuthContext } from './hooks/useAuthContext';
+import Navbar from './components/Navbar';
 
 function App() {
   const { user } = useAuthContext();
 
   return (
     <BrowserRouter>
+      <Navbar/>
       <Routes>
         <Route path="/" element={user ? <Home /> : <SignIn/>} />
         <Route path="/sign-in" element={user ? <Home /> : <SignIn/>} />
