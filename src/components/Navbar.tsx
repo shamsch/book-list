0311ce9fb9a295bde9d16be4f023b22d80eb540f
Shamsur Raza Chowdhury <shamsurraza.chowdhury@tuni.fi>
2022-02-04 04:44:01 +0200

    navbar implmepented

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,15 @@
+import { useAuthContext } from "../hooks/useAuthContext";
 
 
-export default function navbar() {
-  return <div></div>;
+export default function Navbar() {
+  const { user,dispatch } = useAuthContext();
+
+  return (
+    <>
+      <div>
+        <h1>Books to read</h1>
+        {user?<button onClick={()=>dispatch({type: "SIGN_OUT", payload:null})}>Log out</button>:null}
+      </div>
+      {user?<h3>Hello, {user.displayName}</h3>:null}
+    </>);
 }
