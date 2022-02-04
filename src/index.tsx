93879b1c5e520c05cd56a7f38db51f296797de75
Shamsur Raza Chowdhury <shamsurraza.chowdhury@tuni.fi>
2022-02-04 03:09:31 +0200

    context provider wraps the app component

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+//context provider 
+import { AuthContextProvider } from './context/authContext';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <AuthContextProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </AuthContextProvider>
+  ,
   document.getElementById('root')
 );
